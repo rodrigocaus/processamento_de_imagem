a52b357f21f8f0d0a73fb451029a8b19a43139fb
Rodrigo Caus <caus@Bill.localdomain>
2018-06-05 20:44:52 +0000

    Script de teste completo. Faltam algumas verificações

--- test.sh
+++ test.sh
@@ -2,9 +2,22 @@
 
 PASTA_ENTRADA=imgs_test
 PASTA_SAIDA=imgs_out
-EXTENSAO=*.elf
+EXTENSAO=elf
 
-for m in $(ls $EXTENSAO);
+printf "Entrada\t\t"
+printf "Resolução\t"
+printf "Procedimento\t"
+printf "Numero de T/P\t"
+printf "Tempo de operação (ms)\n"
+
+for m in $(ls *.$EXTENSAO);
 do
-	
+	for img in $(ls $PASTA_ENTRADA/)
+	do
+		#Remove as extensões dos arquivos
+		out=${img%.*}
+		processo=${m%.*}
+		processo=${processo#main}
+		./$m $PASTA_ENTRADA/$img $PASTA_SAIDA/$out$processo.jpg
+	done
 done
