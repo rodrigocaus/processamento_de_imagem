cbd742c3ad0f809a18a637e01e32aeb1496cfd5b
Victor Ferrao Santolim <victorfsantolim@gmail.com>
2018-06-05 17:44:54 -0300

    Comecei a implementar a funcao de filtro single. Ainda nao pronta

--- src/filter.c
+++ src/filter.c
@@ -37,7 +37,69 @@ imagem inicializa_saida(imagem *I) {
 }
 
 void aplica_filtro(imagem *I, imagem *O, float **filtro, int ordem) {
-	// TODO
-	for(int i = 0; i < 1000000; i++);
+	
+    //Temp Red, Temp Green e Temp Blue : Acumuladores usados durante a convolucao
+    float tr, tg, tb;
+
+    //Amplitude em pixels em relação ao atual em que a matriz de convolução tem efeito
+    int amp = (int)(ordem/2);
+
+    //Percorre as colunas da imagem
+    for(int y = 0 ; y < (I->height) ; y++)
+    {
+        //Percorre as linhas da imagem
+        for(int x = 0 ; x < (I->width) ; x++)
+        {
+
+            tr = 0;
+            tg = 0;
+            tb = 0;
+
+            //Inicia o valor da convolucao para o pixel [y][x] atual
+            //"me" e "ne" índices esperados dos pixels de conv
+            //"mv" e "nv" índices verdadeiros dos pixels de conv
+            //Para repetir pixels na borda quando sai dos limites
+
+            for(int me = y-amp ; me <= y+amp ; me++)
+            {
+
+                //Não é caso de borda
+                int mv = me;
+
+                //Fora do limite superior
+                if(me >= (I->height)) mv = ((I->height)-1);
+
+                //Fora do limite inferior
+                if(me < 0) mv = 0;
+                
+
+                for(int ne = x-amp ; ne <= x+amp ; ne++)
+                {
+
+                    //Não é caso de borda
+                    int nv = ne;
+
+                    //Fora do limite direito
+                    if(ne >= (I->width)) nv = ((I->width)-1);
+
+                    //Fora do limite esquerdo
+                    if(ne < 0) nv = 0;
+
+                    //Realiza o produto entre o pixel tratado no momento da imagem e da matriz de conv
+
+                    tr = tr + ((I->r)[mv][nv] * filtro[][]);
+                    
+
+                }
+            }
+
+            (O->r)[y][x] = tr;
+            (O->g)[y][x] = tg;
+            (O->b)[y][x] = tb;
+        }
+    }
+	
+
+
 	return;
 }
