906aa4cd69c6877473d1c7f701410a3b4c692687
Rodrigo Caus <caus@Bill.localdomain>
2018-06-06 02:59:03 +0000

    Consertado o padrão de saida. No single foi implementada uma função de cópia de imagem para testar o padrão de saída com o script

--- src/main_single.c
+++ src/main_single.c
@@ -8,6 +8,8 @@
 #define ORDEM 3
 #define NTHREADS 1
 
+void copia_imagem(imagem *i, imagem *o);
+
 float blur[ORDEM][ORDEM] = {1.0/9.0, 1.0/9.0, 1.0/9.0,
 							1.0/9.0, 1.0/9.0, 1.0/9.0,
 							1.0/9.0, 1.0/9.0, 1.0/9.0};
@@ -26,18 +28,29 @@ int main(int argc, char **argv) {
 
 	clock_t t0, t1;
 	t0 = clock();
-	aplica_filtro_single(&entrada, &saida, (float **)blur, ORDEM);
+	//aplica_filtro_single(&entrada, &saida, (float **)blur, ORDEM);
+	copia_imagem(&entrada, &saida);
 	salvar_imagem(argv[2], &saida);
 	t1 = clock();
 
-	printf("%s\t\t", argv[1]);
-	printf("%ux%u\t\t", entrada.width, entrada.height);
-	printf("%s\t\t", "Simples");
+	printf("%s \t\t", argv[1]);
+	printf("%ux%u \t\t", entrada.width, entrada.height);
+	printf("%s\t\t", "single thread");
 	printf("%u\t\t", NTHREADS);
-	printf("%.5f\n", 1000*(double)(t1-t0)/CLOCKS_PER_SEC);
+	printf("%.2f\n", 1000*(double)(t1-t0)/CLOCKS_PER_SEC);
 
 	liberar_imagem(&entrada);
 	liberar_imagem(&saida);
 
 	return 0;
+}
+
+void copia_imagem(imagem *I, imagem *O) {
+	for(int i = 0; i < I->height; i++) {
+		for(int j = 0; j < I->width; j++) {
+			O->r[i][j] = I->r[i][j];
+			O->g[i][j] = I->g[i][j];
+			O->b[i][j] = I->b[i][j];
+		}
+	}
 }
